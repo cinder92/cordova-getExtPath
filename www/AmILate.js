6bb682a7d04d8b939333fb90e17c2f4f8cffabce
Dante Cervantes <cegodai@gmail.com>
2016-03-18 23:08:41 -0500

    no message

--- www/AmILate.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var methods = {
-	coolMethod : function(arg0, success, error) {
-    	exec(success, error, "AmILate", "getNextAppointment", [arg0]);
-    }
-};
-
-module.exports = getSdCard;
